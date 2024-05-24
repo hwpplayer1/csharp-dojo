d85035292e7544c89baf8d3e02a1cac7b7c53c1d
Mert Gör <mertgor@masscollabs.xyz>
2024-05-24 15:50:19 +0300

    Hello World with C#

---
+++ helloworld/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+namespace CSD
+{
+ class App
+ {
+     public static void Main()
+     {
+	 System.Console.WriteLine("Hello World with C#");
+     }
+ }
+}
